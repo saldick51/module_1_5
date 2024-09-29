48f5934db848e1317e7e8373fb820f87664f516e
saldick51 <sal912@yandex.ru>
2024-09-29 09:10:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#immutable_var=(1,3,5,5,7,'ker,vjhrjdm,vfckj')
-#print(type(immutable_var))
-#immutable_var[5]='erhjg'
+immutable_var=(1,3,5,5,7,'ker,vjhrjdm,vfckj')
+print(type(immutable_var))
+immutable_var[5]='erhjg'
 immutable_var=([4,6,3],1)
 print(immutable_var)
 immutable_var[0][2]=8
