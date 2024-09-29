e7520146d20543fafe1e9bdc545d1dd5bc3a051a
saldick51 <sal912@yandex.ru>
2024-09-29 09:08:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#immutable_var=(1,3,5,5,7,'ker,vjhrjdm,vfckj')
+#print(type(immutable_var))
+#immutable_var[5]='erhjg'
+immutable_var=([4,6,3],1)
+print(immutable_var)
+immutable_var[0][2]=8
+print(immutable_var)
